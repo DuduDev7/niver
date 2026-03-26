f4377dd7aa0e657cdc8dc1977e9b2b5719ece510
dudcode7 <eduardo_oliveira37@estudante.sesisenai.org.br>
2026-03-26 11:08:27 -0300

    UPDATE - terminado

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,7 @@
 function login() {
   const senha = document.getElementById("senha").value;
 
-  if (senha === "feliz123") {
+  if (senha === "teAmo") {
     localStorage.setItem("auth", "true");
     window.location.href = "home.html";
   } else {
--- js/hearts.js
+++ js/hearts.js
@@ -6,12 +6,24 @@ c.height = window.innerHeight;
 
 let arr = [];
 
+// Função para criar um coração
+function drawHeart(x, y, size) {
+  ctx.fillStyle = "pink";
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.bezierCurveTo(x, y - size/2, x - size, y - size/2, x - size, y + size/4);
+  ctx.bezierCurveTo(x - size, y + size, x, y + size*1.5, x, y + size*2);
+  ctx.bezierCurveTo(x, y + size*1.5, x + size, y + size, x + size, y + size/4);
+  ctx.bezierCurveTo(x + size, y - size/2, x, y - size/2, x, y);
+  ctx.fill();
+}
+
 function add(){
   arr.push({
     x: Math.random()*c.width,
     y: c.height,
-    s: Math.random()*10+5,
-    v: Math.random()*2+1
+    s: Math.random()*10+5, // tamanho do coração
+    v: Math.random()*2+1   // velocidade
   });
 }
 
@@ -19,13 +31,10 @@ function draw(){
   ctx.clearRect(0,0,c.width,c.height);
 
   arr.forEach((h,i)=>{
-    ctx.fillStyle="pink";
-    ctx.beginPath();
-    ctx.arc(h.x,h.y,h.s,0,Math.PI*2);
-    ctx.fill();
+    drawHeart(h.x, h.y, h.s);
 
     h.y -= h.v;
-    if(h.y<0) arr.splice(i,1);
+    if(h.y < -h.s*2) arr.splice(i,1); // remove quando sai da tela
   });
 
   requestAnimationFrame(draw);
