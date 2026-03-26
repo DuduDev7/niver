714647e8cadc2f8d48df0ddb7f365007c46678c4
dudcode7 <eduardo_oliveira37@estudante.sesisenai.org.br>
2026-03-26 02:47:16 -0300

    UPDATE - terminando

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
 function check() {
-  if (localStorage.getItem("auth") !== "true") {
-    window.location.href = "login.html";
+  if (localStorage.getItem('auth') !== 'true') {
+    window.location.href = 'login.html'
   }
 }
 
 function go(p) {
-  window.location.href = p;
+  window.location.href = p
+}
+
+let imgs = [
+  null, // 👈 primeiro é o "mistério"
+  'imgs/nossa.jpeg',
+  'imgs/missi.jpeg',
+  'imgs/camp.jpeg',
+  'imgs/arthur.jpeg',
+  'imgs/sesi.jpeg'
+]
+
+let textos = ['❤️', '', '', '']
+
+let i = 0
+
+function next() {
+  i = (i + 1) % imgs.length
+
+  const img = document.getElementById('img')
+  const txt = document.getElementById('texto')
+
+  if (imgs[i] === null) {
+    img.style.display = 'none'
+    txt.innerText = textos[i]
+    txt.className = 'misterio-text'
+  } else {
+    img.style.display = 'block'
+    img.src = imgs[i]
+    txt.innerText = ''
+  }
 }
