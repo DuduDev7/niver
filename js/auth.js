09baf6309b392203dfbeb23468e58f18765317bc
Eduardo <duduoliveirafelipe@gmail.com>
2026-05-20 21:55:36 -0300

    UPDATE - hash funcionando

--- js/auth.js
+++ js/auth.js
@@ -1,32 +1,20 @@
-function login() {
+ async function login() {
   const senhaDigitada = document.getElementById("senha").value;
-  const senha = "linda";
+  const senhaHash = "6304458dcb97425679968004dbf8f4e79c7b353813cb734d9491df128833ed79";
 
-  if (senhaDigitada === senha) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(senhaDigitada);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+  if (hashHex === senhaHash) {
     localStorage.setItem("auth", "true");
     window.location.href = "home.html";
   } else {
-    document.getElementById("erro").innerText = "Senha incorreta ❤️";
+    document.getElementById("erro").innerText = "Senha incorreta 🎮";
   }
 }
-
-// async function login() {
-//   const senhaDigitada = document.getElementById("senha").value;
-//   const senhaHash = "9f86d081884c7d6d9ffd60014fc7ee77207e6718d077527338cf9eddbcc90c78"; // hash de "linda"
-
-//   const encoder = new TextEncoder();
-//   const data = encoder.encode(senhaDigitada);
-//   const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-//   const hashArray = Array.from(new Uint8Array(hashBuffer));
-//   const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-
-//   if (hashHex === senhaHash) {
-//     localStorage.setItem("auth", "true");
-//     window.location.href = "home.html";
-//   } else {
-//     document.getElementById("erro").innerText = "Senha incorreta 🎮";
-//   }
-// }
 
 // async function gerarHash(senha) {
 //   const encoder = new TextEncoder();
