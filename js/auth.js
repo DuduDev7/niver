8d328ffa63ccf4bdbf1faac8ae798d0565e64c77
dudcode7 <eduardo_oliveira37@estudante.sesisenai.org.br>
2026-07-26 12:31:44 -0300

    add - new password

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@ async function login() {
 
   const senhaDigitada = document.getElementById("senha").value;
 
-  const senhaHash = "a605387aab9fabfb4cc4ca9d55bb1551a84722885e43554d8a9b038682d16d6f";
+  const senhaHash = "6b09c415ab0568d0cb3a123d7442e505097162c87076f4a12a0aec0dbc64772d";
 
   const encoder = new TextEncoder();
 
